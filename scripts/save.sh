02a043bfb5d5db72ce8dd7127ea7628eb8cde021
Robert Estelle <robertestelle@gmail.com>
2023-01-23 13:28:58 -0800

    save: lint: mark intentionally unreferenced `read` fields

--- scripts/save.sh
+++ scripts/save.sh
@@ -165,7 +165,7 @@ dump_grouped_sessions() {
 		grep "^1" |
 		cut -c 3- |
 		sort |
-		while IFS=$d read session_group session_id session_name; do
+		while IFS=$d read session_group _session_id session_name; do
 			if [[ "$session_group" != "$current_session_group" ]]; then
 				# this session is the original/first session in the group
 				original_session="$session_name"
@@ -192,7 +192,7 @@ fetch_and_dump_grouped_sessions(){
 dump_panes() {
 	local full_command
 	dump_panes_raw |
-		while IFS=$d read line_type session_name window_number window_active window_flags pane_index pane_title dir pane_active pane_command pane_pid history_size; do
+		while IFS=$d read line_type session_name window_number window_active window_flags pane_index pane_title dir pane_active pane_command pane_pid _history_size; do
 			# not saving panes from grouped sessions
 			if is_session_grouped "$session_name"; then
 				continue
@@ -225,7 +225,7 @@ dump_pane_contents() {
 	local pane_contents_area
 	pane_contents_area="$(get_tmux_option "$pane_contents_area_option" "$default_pane_contents_area")"
 	dump_panes_raw |
-		while IFS=$d read line_type session_name window_number window_active window_flags pane_index pane_title dir pane_active pane_command pane_pid history_size; do
+		while IFS=$d read _line_type session_name window_number _window_active _window_flags pane_index _pane_title _dir _pane_active _pane_command _pane_pid history_size; do
 			capture_pane_contents "${session_name}:${window_number}.${pane_index}" "$history_size" "$pane_contents_area"
 		done
 }
