80fa0838391aedfb7a61d6d38ebec3cb3f0d83f5
Robert Estelle <robertestelle@gmail.com>
2023-01-28 14:41:40 -0800

    check_tmux_version: use parameter substituion vs tr

--- scripts/check_tmux_version.sh
+++ scripts/check_tmux_version.sh
@@ -46,10 +46,7 @@ display_message() {
 # `tmux 1.9` => `19`
 # `1.9a`     => `19`
 get_digits_from_string() {
-	local string="$1"
-	local only_digits
-	only_digits="$(echo "$string" | tr -dC '[:digit:]')"
-	echo "$only_digits"
+	echo "${1//[^[:digit:]]/}"
 }
 
 tmux_version_int() {
